97aaf72ce33b141b270ea8d4670c99dc423bd8be
Shree <khape.jai@gmail.com>
2026-08-27 11:37:11 +0530

    Learn SQLite IN BETWEEN and LIKE

---
+++ SQLite/10_in_between_like.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+conn = sqlite3.connect("Hackthon.db")
+cursor = conn.cursor()
+
+cursor.execute(
+    "   SELECT * FROM users WHERE name IN(?, ?, ?)",
+    ("Jai", "Amit", "Arjun"))
+
+users = cursor.fetchall()
+
+for user in users:
+    print(user)
+
+print("-------------")
+
+cursor.execute(
+    "SELECT * FROM users WHERE age BETWEEN ? AND ?",
+    (18, 20))
+
+users = cursor.fetchall()
+
+for user in users:
+    print(user)
+
+print("-------------")
+
+cursor.execute(
+    "SELECT * FROM users WHERE name LIKE ?",
+    ("A%",))
+
+users = cursor.fetchall()
+
+for user in users:
+    print(user)
+
+print("-------------")
+
+conn.close()
