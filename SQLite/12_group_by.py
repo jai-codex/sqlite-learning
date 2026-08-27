2a4ebb495a46734b445fb34eba4f068bb8065a5b
Shree <khape.jai@gmail.com>
2026-08-27 13:46:33 +0530

    Learn SQLite GROUP BY

---
+++ SQLite/12_group_by.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+conn = sqlite3.connect("Hackthon.db")
+cursor = conn.cursor()
+
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS products(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT,
+    Category TEXT,
+    Price INTEGER)""")
+
+products = [
+    ("Rice", "Grain", 50),
+    ("Wheat", "Grain", 40),
+    ("Apple", "Fruit", 120),
+    ("Mango", "Fruit", 100),
+    ("Potato", "Vegetable", 30),
+    ("Tomato", "Vegetable", 40)
+]
+
+cursor.executemany(
+    "INSERT INTO products (Name, Category, Price) VALUES(?, ?, ?)",
+    products)
+
+conn.commit()
+
+print("---------")
+
+cursor.execute(
+    "SELECT Category, COUNT(*) FROM products GROUP BY Category")
+
+results = cursor.fetchall()
+
+for result in results:
+    print(result)
+
+print("---------")
+
+cursor.execute(
+    "SELECT Category, AVG(Price) FROM products GROUP BY Category")
+
+results = cursor.fetchall()
+
+for result in results:
+    print(result)    
+
+conn.close()
