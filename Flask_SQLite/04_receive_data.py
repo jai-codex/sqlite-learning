941dd0ebd9b3810f277491d19ca64d79a6371a64
Shree <khape.jai@gmail.com>
2026-08-29 20:08:38 +0530

    Receive JSON data in Flask

--- Flask_SQLite/04_receive_data.py
+++ Flask_SQLite/04_receive_data.py
@@ -2,16 +2,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/farmers", methods=["GET"])
-def get_farmers():
-    return "Here are the farmers"
-
 @app.route("/farmers", methods=["POST"])
 def add_farmer():
 
     data = request.json
 
-    return f"Farmer received: {data["name"]}"
+    name = data["name"]
+    phone = data["phone"]
+
+    print("Name:", name)
+    print("Phone:", phone)
+
+    return "Farme data received!"
 
 if __name__ == "__main__":
     app.run(debug=True)
